859c1b9e54ef9f8d674b135d029238b548dc4d84
Antonio Piattelli <hello@antoniopiattelli.com>
2024-04-29 12:28:32 +0200

    fixes to structure, cleanup files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import NewsletterSignUp from "./components/NewsletterSignup";
 import React from "react";
 
 const Home: React.FC = () => {
-  return <NewsletterSignUp />;
+  return (
+    <>
+      <NewsletterSignUp />
+    </>
+  );
 };
 
 export default Home;
